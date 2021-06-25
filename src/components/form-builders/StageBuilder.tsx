b04e7fc9ea8c63461c1c3c506fe83b36873bbe76
aTNa <atay.narynov@gmail.com>
2021-06-25 15:56:23 +0600

    Add StageOptions_v2.json; Load <Chains> from API.

--- src/components/form-builders/StageBuilder.tsx
+++ src/components/form-builders/StageBuilder.tsx
@@ -14,50 +14,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
 type RouterParams = { id: string }
+type ChainProps = { id: number, name: string, description: string, campaign: number }
 
 const Builder = () => {
     const [schema, setSchema] = useState('')
     const [uiSchema, setUiSchema] = useState('')
+    const [optionsSchema, setOptionsSchema] = useState({})
+
     const [formResponses, setFormResponses] = useState({})
+    // const [chains, setChains] = useState([])
+
     let {id} = useParams<RouterParams>();
     const history = useHistory();
 
     useEffect(() => {
-        firebase.firestore().collection('stage').doc(id).get().then(doc => {
-            if (doc && doc.exists) {
-                let data = doc.data()
-                if (data) {
-                    if (data.end) {
-                        setSchema(data.end)
-                    }
-                    if (data.end_ui) {
-                        setUiSchema(data.end_ui)
-                    }
-                    setFormResponses(data)
-                }
-            }
-        })
+        axios.get('http://127.0.0.1:8000/api/v1/allchains/')
+            .then(res => res.data)
+            .then(res => {
+                let chains = res
+                let options: any = StageOptions
+                let enumValues: number[] = []
+                let enumNames: string[] = []
+                chains.forEach((chain: ChainProps) => {
+                    enumValues.push(chain.id)
+                    enumNames.push(`${chain.name}: ${chain.description}`)
+                })
+                options.properties.chain.enum = enumValues
+                options.properties.chain.enumNames = enumNames
+                setOptionsSchema(options)
+            })
     }, [id])
 
     const handleSubmit = () => {
-        let json_schema = JSON.parse(schema)
-        let ui_schema = JSON.parse(uiSchema)
+        let json_schema = null
+        let ui_schema = null
+        if (schema && uiSchema) {
+            json_schema = JSON.parse(schema)
+            ui_schema = JSON.parse(uiSchema)
+        }
+
         let data = {...formResponses, json_schema: json_schema, ui_schema: ui_schema}
-        console.log(typeof data)
         console.log(data)
-        // firebase.firestore().collection('stage').doc(id).set(data, {merge: true}).then(() => history.push('/t/' + id))
 
         axios
             .post("http://127.0.0.1:8000/api/v1/alltaskstages/", data)
-            .then((res: any) => {
-                console.log(res)
-                // return res.data
-            })
+            .then((res: any) => console.log(res.data))
             .catch((err: any) => alert(err));
-
-        // console.log(formResponses)
-        // console.log(schema)
-        // console.log(uiSchema)
     }
 
     return (
@@ -72,7 +74,7 @@ const Builder = () => {
             />
             <div style={{width: '70%', minWidth: '400px', margin: '0 auto', display: 'block', padding: 10}}>
                 <Form
-                    schema={StageOptions as JSONSchema7}
+                    schema={optionsSchema as JSONSchema7}
                     formData={formResponses}
                     onChange={(e: { formData: object }) => setFormResponses(e.formData)}
                     onSubmit={handleSubmit}
