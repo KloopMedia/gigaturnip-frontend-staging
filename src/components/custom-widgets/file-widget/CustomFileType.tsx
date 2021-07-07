66750925bae0bc80690782227834ee0c12c8afbf
aTNa <atay.narynov@gmail.com>
2021-07-07 18:50:46 +0600

    Unsuccessful try to add Multiple option to CustomFileType

--- src/components/custom-widgets/file-widget/CustomFileType.tsx
+++ src/components/custom-widgets/file-widget/CustomFileType.tsx
@@ -1,42 +1,39 @@
 import React from 'react'
 
+const CardBody = (props: any) => {
+    const handleChange = (event: any) => {
+        props.onChange({...props.parameters, default: event.target.checked})
+    }
+    return (
+        <div>
+            <input type="checkbox" id="multiple"
+                   value={props.parameters.default}
+                   onChange={handleChange}
+            />
+            <label htmlFor="multiple">Multiple files</label>
+        </div>
+    )
+}
+
 const CustomFileType = {
-        file: {
-            displayName: "File",
-            matchIf: [
-                {
-                    types: ["string"],
-                    widget: "file"
-                },
-            ],
-            defaultDataSchema: {},
-            defaultUiSchema: {
-                "ui:widget": "file"
+    file: {
+        displayName: "File",
+        matchIf: [
+            {
+                types: ["string"],
+                widget: "file"
             },
-            type: "string",
-            // cardBody: (parameters: any, onChange: any) => <div>
-            //     {/*<h5>Multiple files</h5>*/}
-            //     {/*<input*/}
-            //     {/*    value={parameters.default}*/}
-            //     {/*    placeholder="File"*/}
-            //     {/*    type="text"*/}
-            //     {/*    onChange={(ev: any) =>*/}
-            //     {/*        onChange({...parameters, default: ev.target.value})*/}
-            //     {/*    }*/}
-            //     {/*/>*/}
-            //     {console.log(parameters)}
-            //     <input type="checkbox" id="multiple" name="horns"
-            //            value={parameters.multiple}
-            //            onChange={(ev: any) =>
-            //                onChange({...parameters, multiple: ev.target.checked})
-            //            }
-            //     />
-            //     <label htmlFor="multiple">Multiple files</label>
-            // </div>,
-            // modalBody: (parameters: any, onChange: any) => <div>
-            //     Extra editing options in modal appear hear
-            // </div>,
-        }
+        ],
+        defaultDataSchema: {},
+        defaultUiSchema: {
+            "ui:widget": "file"
+        },
+        type: "string",
+        // cardBody: CardBody,
+        // modalBody: (parameters: any, onChange: any) => <div>
+        //     Extra editing options in modal appear hear
+        // </div>,
+    }
 };
 
 export default CustomFileType
--- src/components/custom-widgets/file-widget/CustomFileWidget.tsx
+++ src/components/custom-widgets/file-widget/CustomFileWidget.tsx
@@ -1,16 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
 
 const CustomFileWidget = (props: any) => {
     const {schema} = props;
-
     return (
         <div>
             <label className={"form-label"}>{schema?.title}</label>
             <br/>
             <input type="file"/>
         </div>
-
     )
 }
 
