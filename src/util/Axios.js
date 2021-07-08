d631e1f15ae630b65c1fc3a1f49e4d3ce8b4be01
aTNa <atay.narynov@gmail.com>
2021-07-08 15:51:14 +0600

    Connect to production API

--- src/util/Axios.js
+++ src/util/Axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import {baseUrl} from "./Urls";
 
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: baseUrl
 });
 
 instance.interceptors.request.use(
